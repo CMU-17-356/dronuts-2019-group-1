e9b5e084290253164400471daf9247068b45bedd
Andy Gao <agao@andrew.cmu.edu>
2019-01-25 00:04:59 -0500

    added tests

---
+++ test/homepage.test.js
@@ -0,0 +1,9 @@
+const request = require('supertest')
+const app = require('../app')
+
+describe('homepage', () => {
+  it("welcomes the user", (done) => {
+    request(app).get('/')
+      .expect(200, done)
+  })
+})
